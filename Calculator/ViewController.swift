df10ee4dfd779779be09e0043b513f01c46bb62e
Joseph Sungpil Choi <chousemath@gmail.com>
2017-06-25 13:59:41 +0900

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Global Variables?
+    
+    var usersIsTyping: Bool = false
+    
+    //MARK: Instance Variables
+    
+    @IBOutlet weak var calcDisplay: UILabel!
 
+    //MARK: Actions
+    
+    @IBAction func appendDigit(_ sender: UIButton) {
+        // the ! symbol unwraps the optional String?
+        // app will crash if currentTitle not set (good)
+        let digit = sender.currentTitle!
+        if usersIsTyping {
+            // print("The digit clicked was: \(digit)")
+            calcDisplay.text = calcDisplay.text! + digit
+        } else {
+            calcDisplay.text = digit
+            usersIsTyping = true
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
