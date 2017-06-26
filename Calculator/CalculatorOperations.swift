e8524ef9c8b89c8e738db224e9d87f93d65b5397
Joseph Sungpil Choi <chousemath@gmail.com>
2017-06-26 15:50:16 +0900

    added a few more buttons, started adding model

---
+++ Calculator/CalculatorOperations.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorOperations.swift
+//  Calculator
+//
+//  Created by Joseph Sungpil Choi on 6/26/17.
+//  Copyright © 2017 Joseph Sungpil Choi. All rights reserved.
+//  This is the model component of the MVC pattern
+//
+
+import Foundation
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,16 +13,18 @@ class ViewController: UIViewController {
     //MARK: Global Variables?
     
     var usersIsTyping: Bool = false
+    var charCount: Int = 0
     
     //MARK: Instance Variables
     
     @IBOutlet weak var calcDisplay: UILabel!
-
+    
     //MARK: Actions
     
     @IBAction func appendDigit(_ sender: UIButton) {
         // the ! symbol unwraps the optional String?
         // app will crash if currentTitle not set (good)
+        if (charCount >= 13) { return }
         let digit = sender.currentTitle!
         if usersIsTyping {
             // print("The digit clicked was: \(digit)")
@@ -31,19 +33,47 @@ class ViewController: UIViewController {
             calcDisplay.text = digit
             usersIsTyping = true
         }
-        
+        charCount = calcDisplay.text!.characters.count
+    }
+    
+    var calcDisplayVal: Double {
+        get {
+            return Double(calcDisplay.text!)!
+        }
+        set {
+            calcDisplay.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        usersIsTyping = false
+        if let operatorSymbol = sender.currentTitle {
+            switch operatorSymbol {
+            case "π":
+                let flooredPi: Double = limitCalculation(Double.pi)
+                calcDisplay.text = String(flooredPi)
+            case "√":
+                calcDisplay.text = String(limitCalculation(sqrt(calcDisplayVal)))
+            default:
+                break
+            }
+        }
+    }
+    
+    func limitCalculation(_ a: Double) -> Double {
+        return floor(a * 100000000) / 100000000.0
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
